21ea6d0cb06bedb8e8fac8967c27cd8d07fb775d
Molly Swanson <molly.swanson@gmail.com>
2011-12-22 15:01:02 -0500

    updating to version 0.1 --Molly

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 setup(name='longdouble_utils',
       version='0.1',
       py_modules=['longdouble_utils'],
-      description='Basic utility functions for converting between strings and np.float128 floating point numbers',
+      install_requires=['numpy','mpmath'],
+      description='Basic utility functions for converting between strings and np.float128 floating point numbers.',
       author='Molly Swanson',
       author_email='molly.swanson@gmail.com',
       url='https://github.com/mollyswanson/longdouble_utils'
